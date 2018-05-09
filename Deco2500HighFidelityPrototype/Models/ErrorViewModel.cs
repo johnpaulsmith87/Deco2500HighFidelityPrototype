c4160b819755743c6d2f4dfd3c8c63ca71e99713
johnpaulsmith87 <johnpaul.smith@gmail.com>
2018-05-09 12:22:05 +1000

    Add project files.

---
+++ Deco2500HighFidelityPrototype/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Deco2500HighFidelityPrototype.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
